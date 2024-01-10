cb2bdeeddd9ee71124de0e61afb9cf88db1deac4
jdann1hirix <145584635+jdann1hirix@users.noreply.github.com>
2024-01-10 16:05:09 -0300

    Create User Schema and defined post user endpoint

--- user.py
+++ user.py
@@ -1,38 +1,51 @@
 from datetime import datetime
+import uuid
 
 def get_created_at_timestamp():
-    return "2024-01-10 15:29:00"
+    return "2024-01-10T15:29:00"
 
 def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+    return datetime.now().strftime(("%Y-%m-%dT%H:%M:%S"))
 
 USER = {
     "Fairy": {
         "uuid": "e340d0fa-3f91-4ca8-913d-e47c5d7d35c6",
-        "name": "Tooth",
         "login": "Fairy",
         "created_at": get_created_at_timestamp(),
         "modified_at": get_created_at_timestamp(),
-        "user_relationship": "3d402b31-e1f6-4b89-979e-4de0537f31c6",
+        "user_relationship_id": "3d402b31-e1f6-4b89-979e-4de0537f31c6",
 
     },
     "Ruprecht": {
         "uuid": "3d402b31-e1f6-4b89-979e-4de0537f31c6",
-        "name": "Knecht",
         "login": "Ruprecht",
         "created_at": get_created_at_timestamp(),
         "modified_at": get_created_at_timestamp(),
-        "user_relationship": "2bd31a35-c282-4c33-859e-cb89cb567498",
+        "user_relationship_id": "2bd31a35-c282-4c33-859e-cb89cb567498",
     },
     "Bunny": {
         "uuid": "2bd31a35-c282-4c33-859e-cb89cb567498",
-        "name": "Easter",
         "login": "Bunny",
         "created_at": get_created_at_timestamp(),
         "modified_at": get_created_at_timestamp(),
-        "user_relationship": "null",
+        "user_relationship_id": "null",
     }
 }
 
 def read_all():
     return list(USER.values())
+
+def create(user):
+    uuidnumber = str(uuid.uuid4())
+    login = user.get("login")
+    user_relationship_id = user.get("user_relationship_id", "")
+
+    USER[uuidnumber] = {
+            "uuid": uuidnumber,
+            "login": login,
+            "user_relationship_id": user_relationship_id,
+            "created_at": get_timestamp(),
+            "modified_at": get_timestamp(),
+    }
+
+    return USER[uuidnumber], 201
